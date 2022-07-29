17c00ad6811debeecf4b923cf643d2715d9b05a2
Minh Pham <thanhminh1602@gmail.com>
2022-07-29 16:34:39 +0700

    commit first

---
+++ beplus-sticky-language-fluentu.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Beplus Sticky Language Fluentu
+ * Plugin URI:
+ * Description: Sticky Header Languages for Fluentu site
+ * Version: 1.0
+ * Author: Beplus
+ * Author URI:
+ */
