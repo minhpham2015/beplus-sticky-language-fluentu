cee4945915d7b585be65e7e782e1bb963e824a60
Minh Pham <thanhminh1602@gmail.com>
2022-07-30 10:42:47 +0700

    fix struct

--- beplus-sticky-language-fluentu.php
+++ beplus-sticky-language-fluentu.php
@@ -1,9 +1,7 @@
 <?php
 /**
  * Plugin Name: Beplus Sticky Language Fluentu
- * Plugin URI:
  * Description: Sticky Header Languages for Fluentu site
  * Version: 1.0
  * Author: Beplus
- * Author URI:
  */
